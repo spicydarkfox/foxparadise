63fe255ccc63392dbc869cea7beb87aba49902c5
SpicyDarkFox <128318785+SpicyDarkFox@users.noreply.github.com>
2026-05-02 21:45:10 +0300

    Консоль дисков больше не делает Т3 (#259)

--- Content.Server/Research/TechnologyDisk/Systems/DiskConsoleSystem.cs
+++ Content.Server/Research/TechnologyDisk/Systems/DiskConsoleSystem.cs
@@ -6,6 +6,7 @@ using Content.Shared.Research.Components;
 using Robust.Server.Audio;
 using Robust.Server.GameObjects;
 using Robust.Shared.Timing;
+using Robust.Shared.Random; //LP edit
 
 namespace Content.Server.Research.TechnologyDisk.Systems;
 
@@ -15,6 +16,7 @@ public sealed class DiskConsoleSystem : EntitySystem
     [Dependency] private readonly AudioSystem _audio = default!;
     [Dependency] private readonly ResearchSystem _research = default!;
     [Dependency] private readonly UserInterfaceSystem _ui = default!;
+    [Dependency] private readonly IRobustRandom _random = default!; //LP edit
 
     /// <inheritdoc/>
     public override void Initialize()
@@ -38,7 +40,11 @@ public sealed class DiskConsoleSystem : EntitySystem
                 continue;
 
             RemComp(uid, printing);
-            Spawn(console.DiskPrototype, xform.Coordinates);
+            //LP edit start
+            var diskPrototypes = new[] { "TechnologyDiskT1", "TechnologyDiskT2" };
+            var chosenPrototype = _random.Pick(diskPrototypes);
+            //LP edit end
+            Spawn(chosenPrototype, xform.Coordinates);  //LP edit
         }
     }
 
